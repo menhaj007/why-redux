b3d200983b65b9724620a37ec83ce5e08a59b353
Menhajuddin Sharaf <menhaj007@yahoo.com>
2021-09-16 01:05:23 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Welcome to <code>Welcome to Fort McCoy</code> and save to reload.
         </p>
         <a
           className="App-link"
